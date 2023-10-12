640525b38b4e72f75d549b9e0857936a572d05f6
Fabricio Borgobello <fborgobello@gmail.com>
2023-10-12 16:28:26 +0200

    Remove JSON DB and improve error messages

--- js/index.js
+++ js/index.js
@@ -19,32 +19,36 @@ async function addToCart(product, quantity = 1) {
 }
 
 async function displayProductList() {
-  const products = await getProducts();
   let list = document.getElementById("product-list");
-  products.forEach((product) => {
-    let item = document.createElement("div");
-    item.innerHTML =
-      /* HTML */
-      `<div class="product-card">
-        <img
-          class="product-image"
-          src="${product.image}"
-          alt="${product.name}"
-        />
-        <h2 class="product-name">
-          <a href="product.html?id=${product.id}">${product.name}</a>
-        </h2>
-        <p class="product-price">$ ${product.price}</p>
-        <button class="add-to-cart-btn">Add to Cart</button>
-      </div>`;
-    list.appendChild(item);
+  try {
+    const products = await getProducts();
+    products.forEach((product) => {
+      let item = document.createElement("div");
+      item.innerHTML =
+        /* HTML */
+        `<div class="product-card">
+          <img
+            class="product-image"
+            src="${product.image}"
+            alt="${product.name}"
+          />
+          <h2 class="product-name">
+            <a href="product.html?id=${product.id}">${product.name}</a>
+          </h2>
+          <p class="product-price">$ ${product.price}</p>
+          <button class="add-to-cart-btn">Add to Cart</button>
+        </div>`;
+      list.appendChild(item);
 
-    // Add to cart button
-    let addToCartBtn = item.querySelector(".add-to-cart-btn");
-    addToCartBtn.addEventListener("click", () => {
-      addToCart(product);
+      // Add to cart button
+      let addToCartBtn = item.querySelector(".add-to-cart-btn");
+      addToCartBtn.addEventListener("click", () => {
+        addToCart(product);
+      });
     });
-  });
+  } catch (error) {
+    list.innerHTML = `<p class="error">An error occurred fetching the products. Please check if the API is running.</p>`;
+  }
 }
 
 displayProductList();
